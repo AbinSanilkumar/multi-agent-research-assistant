9c6248f7328904ea68b569509d71c8eb807ff0c1
Abin <abi903719@gmail.com>
2026-06-14 22:55:51 +0530

    Setup Django apps and basic authentication pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register_view(request):
+    return render(request, 'accounts/register.html')
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
--- research/admin.py
+++ research/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# research/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import ResearchSession
+
+admin.site.register(ResearchSession)
